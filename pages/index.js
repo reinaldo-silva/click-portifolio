277e66a649bfdfb6d744bc01980f0c71deed7e42
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-05 13:39:55 -0300

    Alterando a página home

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Link from "next/link";
+import { GlobalStyles } from "../styles/global.js";
+import { Container, Title, Text } from "../styles/index.js";
+import GoogleFonts from "next-google-fonts";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Página Home</h1>
-      <Link href="/sobre">
-        <a>Acessar Sobre</a>
-      </Link>
-    </div>
+    <>
+      <GlobalStyles />
+      <GoogleFonts
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <Container>
+        <Image
+          src="/me.jpg"
+          alt="Picture of the author"
+          width={180}
+          height={180}
+        />
+        <Title>Developer Web</Title>
+        <Text>Reinaldo Luiz da SIlva Junior</Text>
+        <Text>reinaldo.silva.jr@outlook.com</Text>
+      </Container>
+    </>
   );
 };
 
