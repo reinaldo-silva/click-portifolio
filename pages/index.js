2b0fd13ec73af10b57a5a35fe4e1a6d77e3db7b1
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-04 22:24:56 -0300

    Adicionando página sobre

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Página teste</div>;
+  return (
+    <div>
+      <h1>Página Home</h1>
+      <Link href="/sobre">
+        <a>Acessar Sobre</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Sobre = () => {
+  return (
+    <div>
+      <h1>Página teste</h1>
+      <Link href="/">
+        <a>Acessar Home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sobre;
