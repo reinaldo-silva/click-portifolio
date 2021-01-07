9a6a3e049348e999885800e6a8d85c6b2c112fca
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-01-07 13:13:03 -0300

    Criando paginas de navegação web

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,25 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className="flex justify-center md:justify-between py-2 md:py-4 items-center">
-      <motion.h1 className="">Developer web</motion.h1>
+      <Link href="/">
+        <a>
+          <motion.h1 className="font-bold text-3xl cursor-">
+            Developer web
+          </motion.h1>
+        </a>
+      </Link>
 
       <nav className="hidden md:block space-x-8">
-        <Link href="/projetos">
+        <Link href="/projects">
           <a className="tracking-wide hover:text-gray-300">Projetos</a>
         </Link>
-        <Link href="/sobre">
+        <Link href="/about">
           <a className="tracking-wide hover:text-gray-300">Sobre</a>
         </Link>
-        <Link href="/contato">
+        <Link href="/contact">
           <a className="tracking-wide hover:text-gray-300">Contato</a>
         </Link>
       </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
+import Link from "next/link";
+
+interface CardProps {
+  href: string;
+  imgSrc: string;
+  layoutId: string;
+}
+
+const Card: React.FC<CardProps> = ({ href, imgSrc, layoutId }) => {
+  return (
+    <Link href={href}>
+      <motion.div
+        variants={variants}
+        className="relative h-20 h-40 bg-gray-300 p-4  rounded-xl cursor-pointer hover:bg-gray-100 transition"
+      >
+        <motion.img
+          src={imgSrc}
+          className="w-full h-full"
+          layoutId={layoutId}
+        />
+        {/* <motion.span>Texto</motion.span> */}
+      </motion.div>
+    </Link>
+  );
+};
+
+const variants: Variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delayChildren: 1,
+      staggerChildren: 0.7,
+    },
+  },
+};
 
 const Home = () => {
   return (
     <>
-      <motion.div className="mt-24 grid w-full grid-cols-1 grid-rows-2 gap-4">
+      <motion.div className="mt-32 grid w-full grid-cols-1 grid-rows-2 gap-4">
         <motion.div className="relative flex justify-center transition">
           <motion.img
             src="/me.jpg"
@@ -13,9 +50,36 @@ const Home = () => {
         </motion.div>
 
         <motion.div className="flex justify-center items-center flex-col">
-          <motion.p>Reinaldo Luiz da SIlva Junior</motion.p>
-          <motion.p>reinaldo.silva.jr@outlook.com</motion.p>
+          <motion.p className="mb-4 font-semibold text-2xl">
+            Reinaldo Luiz da Silva Junior
+          </motion.p>
         </motion.div>
+      </motion.div>
+
+      <motion.div
+        className="mt-16 grid w-full grid-cols-1 grid-rows-3 gap-4 md:grid-cols-3 md:grid-rows-1"
+        /*         transition={{
+          delay: 2,
+        }}
+        variants={variants}
+        initial="hidden"
+        animate="visible" */
+      >
+        <Card
+          imgSrc={"/img/dev.svg"}
+          href={"/projects"}
+          layoutId={"projetos-logo"}
+        />
+        <Card
+          imgSrc={"/img/info.svg"}
+          href={"/about"}
+          layoutId={"sobre-logo"}
+        />
+        <Card
+          imgSrc={"/img/contact.svg"}
+          href={"/contact"}
+          layoutId={"contato-logo"}
+        />
       </motion.div>
     </>
   );
